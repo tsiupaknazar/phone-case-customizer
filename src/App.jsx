5700e28b990e79c248ab7c9e1cde5ae46ef9702e
tsiupaknazar <sjprime280@gmail.com>
2023-10-07 23:18:47 +0200

    Experimenting with view settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import './App.css'
+import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./components/Experience";
 import Configurator from "./components/Configurator";
 import { CustomizationProvider } from "./context/Customization";
 
 function App() {
-
   return (
     <CustomizationProvider>
       <div className="App">
-        <Canvas dpr={[1, 2]}>
-          <color attach="background" args={["#213547"]} />
-          <fog attach="fog" args={["#213547", 10, 20]} />
+        <Canvas style={{ background: "#171717" }}>
+          <ambientLight intensity={1} />
+          <spotLight
+            intensity={0.5}
+            angle={0.1}
+            penumbra={1}
+            position={[10, 15, 10]}
+            castShadow
+          />
           <Experience />
         </Canvas>
         <Configurator />
       </div>
     </CustomizationProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Case.jsx
+++ src/components/Case.jsx
@@ -1,33 +1,36 @@
 import React, { useRef } from "react";
-import { useGLTF, useTexture, Decal } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
 
 import * as THREE from "three";
 import { useCustomization } from "../context/Customization";
 
 function Case(props) {
   const { nodes, materials } = useGLTF("./models/case.gltf");
-  const { caseColor, caseImage } = useCustomization();
+  const { caseColor, caseImage } = useCustomization(); // Add caseImage from context
   return (
     <group {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
-        {/* Create the 3D model parts with their original color */}
+        {/* Create a decal by using a transparent texture */}
         <mesh
           geometry={nodes.Object_2.geometry}
           material={materials["Material.006"]}
-        />
+        >
+          <meshStandardMaterial color={caseColor.color} />
+        </mesh>
         <mesh
           geometry={nodes.Object_3.geometry}
           material={materials["Material.007"]}
-        />
-
-        {/* Add the image as a texture to the 3D model */}
-        {caseImage.url && (
-          <mesh geometry={nodes.Object_3.geometry}>
-            <meshBasicMaterial
-              map={new THREE.TextureLoader().load(caseImage.url)}
-            />
-          </mesh>
-        )}
+        >
+          <meshStandardMaterial color={caseColor.color} />
+          {caseImage.url && (
+            <mesh geometry={nodes.Object_3.geometry}>
+              <meshBasicMaterial
+                transparent
+                map={new THREE.TextureLoader().load(caseImage.url)}
+              />
+            </mesh>
+          )}
+        </mesh>
       </group>
     </group>
   );
